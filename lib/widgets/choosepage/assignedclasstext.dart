76ef17d16cd6af46e872d5290affbc013c60fe6f
pentaCoxian <c241494r@icu.ac.jp>
2023-10-15 00:25:38 +0900

    change UI for course removal

--- lib/widgets/choosepage/assignedclasstext.dart
+++ lib/widgets/choosepage/assignedclasstext.dart
@@ -7,8 +7,15 @@ import 'package:icuapp/model/constant.dart';
 import 'dart:async' show Future;
 import 'package:icuapp/widgets/choosepage/showlist.dart';
 
-class AssignedClassText extends ConsumerWidget {
+class AssignedClassText extends ConsumerStatefulWidget {
   const AssignedClassText({Key? key}) : super(key: key);
+
+  @override
+  AssignedClassTextState createState() => AssignedClassTextState();
+}
+
+class AssignedClassTextState extends ConsumerState<AssignedClassText> {
+  int currentIndex = 0; // Add this variable to keep track of the current index
 
   Future getCourseInfo(int year, String season, String period_day) async {
     final CourseInfo noneFoundData = CourseInfo()
@@ -23,7 +30,12 @@ class AssignedClassText extends ConsumerWidget {
   }
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     final chosenYear = ref.watch(chosenYearProvider);
     final chosenSeason = ref.watch(chosenSeasonProvider);
     final fontSize = ref.watch(cellFontSizeProvider);
@@ -37,6 +49,8 @@ class AssignedClassText extends ConsumerWidget {
         if (snapshot.hasData) {
           CourseInfo chosenData = snapshot.data!;
           List selectedList = [chosenData];
+          selectedList.add(noneFoundData);
+
           return Card(
             color: Colors.white,
             shape: RoundedRectangleBorder(
@@ -45,43 +59,37 @@ class AssignedClassText extends ConsumerWidget {
             margin: const EdgeInsets.fromLTRB(8, 40, 8, 8),
             child: Padding(
               padding: const EdgeInsets.fromLTRB(16.0, 6, 10, 10),
-              child: Container(
-                decoration: const BoxDecoration(
-                  border: Border(
-                    bottom: BorderSide(width: 1.0, color: Colors.black26),
-                  ),
-                ),
-                child: ListView.builder(
-                  padding: EdgeInsets.zero,
-                  itemCount: 1,
-                  shrinkWrap: true,
-                  itemBuilder: (context, index) {
-                    final item = selectedList[index];
-                    return (chosenData.no != '' &&
-                            chosenData.j != 'Tap here to reset')
-                        ? Dismissible(
-                            key: UniqueKey(),
-                            confirmDismiss: (direction) {
-                              selectedList.removeAt(index);
-                              return Future<bool>.value(true);
-                            },
-                            onDismissed: (direction) {
-                              IsarService()
-                                  .deleteCourseFromTT(item.courseId, ref);
-                              ScaffoldMessenger.of(context).showSnackBar(
-                                SnackBar(
-                                  content:
-                                      Text('${item.j} / ${item.e} removed!'),
-                                ),
-                              );
-                            },
-                            background:
-                                Container(color: const Color(0xFFD71A1A)),
-                            child:
-                                ListTileTxtInfo(selectedList[index], fontSize))
-                        : ListTileTxtInfo(noneFoundData, fontSize);
-                  },
-                ),
+              child: ListView.builder(
+                padding: EdgeInsets.zero,
+                itemCount: 1,
+                shrinkWrap: true,
+                itemBuilder: (context, index) {
+                  final item = selectedList[currentIndex];
+                  return (chosenData.no != '' &&
+                          chosenData.j != 'Tap here to reset')
+                      ? Dismissible(
+                          key: UniqueKey(),
+                          confirmDismiss: (direction) {
+                            selectedList.removeAt(currentIndex);
+                            return Future<bool>.value(true);
+                          },
+                          onDismissed: (direction) {
+                            IsarService()
+                                .deleteCourseFromTT(item.courseId, ref);
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(
+                                content: Text('${item.j} / ${item.e} removed!'),
+                              ),
+                            );
+                            currentIndex = 1;
+                            setState(() {});
+                          },
+                          background: Container(color: const Color(0xFFD71A1A)),
+                          child: ListTileTxtInfo(
+                              selectedList[currentIndex], fontSize),
+                        )
+                      : ListTileTxtInfo(noneFoundData, fontSize);
+                },
               ),
             ),
           );
